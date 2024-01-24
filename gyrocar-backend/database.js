b8e0c81be70e31d59d2525ec3482c6ff611a6097
Riley Basile-Benson <rileybenson543@gmail.com>
2024-01-24 14:49:33 -0500

    Addding database coonnection

---
+++ gyrocar-backend/database.js
@@ -0,0 +1,12 @@
+const db = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : 'db-1.ckkvuqjth30g.us-east-2.rds.amazonaws.com',
+      port : 3306,
+      user : 'admin',
+      password : 'tZtp9E7n7J4j2o3aIv17',
+      database : 'GyroCar'
+    }
+  });
+
+module.exports = db;
